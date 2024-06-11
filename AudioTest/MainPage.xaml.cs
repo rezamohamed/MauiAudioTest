fcdba6aafb663a371f73098f55e7e39edad567a2
Reza Mohamed <reza.mohamed@outlook.com>
2024-06-11 15:46:12 -0500

    Initial commit

---
+++ AudioTest/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Plugin.Maui.Audio;
+
+namespace AudioTest;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IAudioManager _audioManager;
+
+    private IAudioRecorder _audioRecorder;
+    
+    public MainPage(IAudioManager audioManager)
+    {
+        _audioManager = audioManager;
+        InitializeComponent();
+    }
+    
+
+    private async void RecordBtn_OnClicked(object? sender, EventArgs e)
+    {
+        try
+        {
+            _audioRecorder = _audioManager.CreateRecorder();
+
+            if (await Permissions.RequestAsync<Permissions.Microphone>() == PermissionStatus.Granted)
+            {
+                
+                if (!_audioRecorder.IsRecording)
+                {
+                    
+
+                    var options = new AudioRecordingOptions
+                    {
+                        SampleRate = AudioRecordingOptions.DefaultSampleRate,
+                        //SampleRate = 16000,
+                        Channels = AudioRecordingOptions.DefaultChannels,
+                        //Channels = ChannelType.Stereo,
+                        BitDepth = AudioRecordingOptions.DefaultBitDepth,
+                        //BitDepth = BitDepth.Pcm16bit,
+                        Encoding = AudioRecordingOptions.DefaultEncoding,
+                        //Encoding = Encoding.Alac,
+                        ThrowIfNotSupported = true
+                    };
+                    
+                    
+                    await _audioRecorder.StartAsync(options);
+                    //await audioRecorder.StartAsync();
+                }
+                
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
+    }
+
+    private async void StopBtn_OnClicked(object? sender, EventArgs e)
+    {
+        var recordedAudio = await _audioRecorder.StopAsync();
+
+        // basic player - doesn't work in Android
+        //var player = AudioManager.Current.CreatePlayer(recordedAudio.GetAudioStream());
+
+        byte[] recordedBytes;
+        
+        using (var stream = new MemoryStream())
+        {
+            await recordedAudio.GetAudioStream().CopyToAsync(stream);
+            recordedBytes = stream.ToArray();
+        }
+
+
+        // this is for testing
+        MemoryStream test = new MemoryStream(recordedBytes);
+        var player = AudioManager.Current.CreatePlayer(test);
+        player.Play();
+        //testing end
+        
+    }
+}
---
+++ AudioTest/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
+
+namespace AudioTest;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .AddAudio(
+                playbackOptions =>
+                {
+#if IOS || MACCATALYST
+                    playbackOptions.Category = AVFoundation.AVAudioSessionCategory.Playback;
+#endif
+                },
+                recordingOptions =>
+                {
+#if IOS || MACCATALYST
+                    recordingOptions.Category = AVFoundation.AVAudioSessionCategory.Record;
+                    recordingOptions.Mode = AVFoundation.AVAudioSessionMode.Default;
+                    recordingOptions.CategoryOptions = AVFoundation.AVAudioSessionCategoryOptions.MixWithOthers;
+#endif
+                });
+
+
+        builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
